22c7b37a9d9ee941e1d597cc461f13926a9c259b
ArturLLopes <arturllopes@gmail.com>
2025-05-20 10:35:26 -0300

    Start: Inicio do projeto Conta Bancaria

---
+++ src/main/java/com/projeto/simulador/SimuladorContaBancariaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.simulador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorContaBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorContaBancariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/simulador/SimuladorContaBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.simulador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorContaBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
